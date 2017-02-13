aee3d5bf70f0ead4f52dcdc3c0f7f777480ab92b
Linus Gasser <linus.gasser@epfl.ch>
2017-02-13 10:55:13 +0100

    debugging travis

--- coveralls.sh
+++ coveralls.sh
@@ -15,7 +15,7 @@ all_tests_passed=true
 echo "mode: atomic" > profile.cov
 for dir in $DIR_SOURCE; do
 	if ! echo $DIR_EXCLUDE | grep -q $dir; then
-	    go test -short -race -covermode=atomic -coverprofile=$dir/profile.tmp $dir
+	    go test -short -race -covermode=atomic -coverprofile=$dir/profile.tmp -v $dir
 
     	if [ $? -ne 0 ]; then
         	all_tests_passed=false
