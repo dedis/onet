02c590e4db1473a6e0d8f42d30b925c17aca2fb2
Linus Gasser <linus.gasser@epfl.ch>
2017-03-31 13:40:56 +0200

    adding mutex for testing

--- context.go
+++ context.go
@@ -127,6 +127,7 @@ func (c *Context) String() string {
 }
 
 var contextData = map[string][]byte{}
+var contextDataLock = sync.Mutex{}
 
 // Save takes an identifier and an interface. The interface will be network.Marshaled
 // and saved under a filename based on the identifier. An eventual error will be returned.
@@ -149,7 +150,9 @@ func (c *Context) Save(id string, data interface{}) error {
 	}
 	fname := c.absFilename(id)
 	if getContextDataPath() == "" {
+		contextDataLock.Lock()
 		contextData[fname] = buf
+		contextDataLock.Unlock()
 		return nil
 	}
 	return ioutil.WriteFile(fname, buf, 0640)
@@ -166,7 +169,9 @@ func (c *Context) Load(id string) (interface{}, error) {
 	var buf []byte
 	if getContextDataPath() == "" {
 		var ok bool
+		contextDataLock.Lock()
 		buf, ok = contextData[c.absFilename(id)]
+		contextDataLock.Unlock()
 		if !ok {
 			return nil, errors.New("This entry doesn't exist")
 		}
@@ -185,7 +190,9 @@ func (c *Context) Load(id string) (interface{}, error) {
 // contextData map.
 func (c *Context) DataAvailable(id string) bool {
 	if getContextDataPath() == "" {
+		contextDataLock.Lock()
 		_, ok := contextData[c.absFilename(id)]
+		contextDataLock.Unlock()
 		return ok
 	}
 	_, err := os.Stat(c.absFilename(id))
