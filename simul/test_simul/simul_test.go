e06b25b3ec8eb9556dd1f36781cf37c4776e8d78
Linus Gasser <linus.gasser@epfl.ch>
2017-09-14 09:36:30 +0200

    removing unneeded test-files

--- simul/test_simul/simul_test.go
+++ simul/test_simul/simul_test.go
@@ -10,5 +10,5 @@ func TestSimulation(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Not running test because travis doesn't have sudo")
 	}
-	simul.Start("count.toml", "csv1.toml", "csv2.toml")
+	simul.Start("count.toml")
 }
