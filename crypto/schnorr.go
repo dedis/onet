9fe2cbde179594ae5c0daa9ee10732f64d7c16cb
Linus Gasser <linus.gasser@epfl.ch>
2017-09-27 09:45:04 +0200

    even more simple

--- crypto/schnorr.go
+++ crypto/schnorr.go
@@ -6,40 +6,14 @@ import (
 )
 
 // SchnorrSig is a signature created using the Schnorr Signature scheme.
-type SchnorrSig struct {
-	Challenge abstract.Point
-	Response  abstract.Scalar
-}
+type SchnorrSig []byte
 
 // SignSchnorr creates a Schnorr signature from a msg and a private key
 func SignSchnorr(suite abstract.Suite, private abstract.Scalar, msg []byte) (SchnorrSig, error) {
-	ss := SchnorrSig{
-		Challenge: suite.Point(),
-		Response:  suite.Scalar(),
-	}
-	buf, err := sign.Schnorr(suite, private, msg)
-	if err != nil {
-		return ss, err
-	}
-	scalarLen := suite.ScalarLen()
-	if err := ss.Challenge.UnmarshalBinary(buf[0:scalarLen]); err != nil {
-		return ss, err
-	}
-	if err := ss.Response.UnmarshalBinary(buf[scalarLen:]); err != nil {
-		return ss, err
-	}
-	return ss, nil
+	return sign.Schnorr(suite, private, msg)
 }
 
 // VerifySchnorr verifies a given Schnorr signature. It returns nil iff the given signature is valid.
 func VerifySchnorr(suite abstract.Suite, public abstract.Point, msg []byte, sig SchnorrSig) error {
-	ch, err := sig.Challenge.MarshalBinary()
-	if err != nil {
-		return err
-	}
-	re, err := sig.Response.MarshalBinary()
-	if err != nil {
-		return err
-	}
-	return sign.VerifySchnorr(suite, public, msg, append(ch, re...))
+	return sign.VerifySchnorr(suite, public, msg, sig)
 }
