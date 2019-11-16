48ffa9fae4c33773319f99b2183ffdd6ade42d8b
BjornGudmundsson <bjorn.gudmundsson95@gmail.com>
2019-11-16 21:44:36 +0000

    All tests pass, the new hope crypto suite works

--- ciphersuite/newHope_test.go
+++ ciphersuite/newHope_test.go
@@ -156,3 +156,44 @@ func TestNewHopeCipherSuite_GenerateKey(t *testing.T) {
 	require.NotNil(t, pk)
 	require.NotNil(t, sk)
 }
+
+func TestNewHopeCipherSuite_Sign(t *testing.T) {
+	suite := NewNewHopeCipherSuite()
+
+	_, sk, err := suite.GenerateKeyPair(nil)
+	require.NoError(t, err)
+
+	sig, err := suite.Sign(sk, []byte{})
+	require.NoError(t, err)
+	require.NotNil(t, sig)
+
+	rawSk := sk.Raw()
+	rawSk.CipherName = "abc"
+
+	_, err = suite.Sign(rawSk, []byte{})
+	require.Error(t, err)
+}
+
+func TestNewHopeCipherSuite_Verify(t *testing.T) {
+	suite := NewNewHopeCipherSuite()
+
+	pk, sk, err := suite.GenerateKeyPair(nil)
+	require.NoError(t, err)
+
+	sig, err := suite.Sign(sk, []byte{})
+	require.NoError(t, err)
+
+	err = suite.Verify(pk, sig, []byte{})
+	require.NoError(t, err)
+
+	rawSig := sig.Raw()
+	rawSig.CipherName = "abc"
+	err = suite.Verify(pk, rawSig, []byte{})
+	require.Error(t, err)
+
+	rawPk := pk.Raw()
+	rawPk.CipherName = "abc"
+	err = suite.Verify(rawPk, sig, []byte{})
+	require.Error(t, err)
+	fmt.Println("Bjorinn")
+}
